192d9ada72e75d89a01bc25f43652a18233cc75f
talgat <talgat.baltasov@gmail.com>
2020-07-08 22:31:00 +0600

    fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -94,18 +94,6 @@
                                 <li class="menu-item-has-children active">
                                     <a href="/">Главная</a>
                                 </li>
-                                @foreach($categories as $category)
-                                    <li class="menu-item-has-children">
-                                        <a href="/c/{{$category->slug}}">{{$category->name}}</a>
-                                        @if($category->children->count() > 0)
-                                            <ul class="sub-menu">
-                                                @foreach($category->children as $child)
-                                                    <li><a href="/c/{{$child->slug}}">{{$child->name}}</a></li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </li>
-                                @endforeach
                                 <li class="menu-item-has-children">
                                     <a href="/payments-delivery">Доставка и оплата</a>
                                 </li>
@@ -290,122 +278,27 @@
                         <div class="col-lg-3">
                             <div class="categories_menu">
                                 <div class="categories_title">
-                                    <h2 class="categori_toggle">Categories</h2>
+                                    <h2 class="categori_toggle">Категории</h2>
                                 </div>
                                 <div class="categories_menu_toggle">
                                     <ul>
-                                        <li class="menu_item_children"><a href="#">Brake Parts <i class="fa fa-angle-right"></i></a>
-                                            <ul class="categories_mega_menu">
-                                                <li class="menu_item_children"><a href="#">Dresses</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Sweater</a></li>
-                                                        <li><a href="">Evening</a></li>
-                                                        <li><a href="">Day</a></li>
-                                                        <li><a href="">Sports</a></li>
+                                        @foreach($categories as $category)
+                                            @if($category->children->count() > 0)
+                                                <li class="menu_item_children"><a href="/c/{{$category->slug}}">{{$category->name}} <i class="fa fa-angle-right"></i></a>
+                                                    <ul class="categories_mega_menu">
+                                                        <li class="menu_item_children">
+                                                            <ul class="categorie_sub_menu">
+                                                                @foreach($category->children as $child)
+                                                                    <li><a href="/c/{{$child->slug}}">{{$child->name}}</a></li>
+                                                                @endforeach
+                                                            </ul>
+                                                        </li>
                                                     </ul>
                                                 </li>
-                                                <li class="menu_item_children"><a href="#">Handbags</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Shoulder</a></li>
-                                                        <li><a href="">Satchels</a></li>
-                                                        <li><a href="">kids</a></li>
-                                                        <li><a href="">coats</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">shoes</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Ankle Boots</a></li>
-                                                        <li><a href="">Clog sandals </a></li>
-                                                        <li><a href="">run</a></li>
-                                                        <li><a href="">Books</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">Clothing</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Coats  Jackets </a></li>
-                                                        <li><a href="">Raincoats</a></li>
-                                                        <li><a href="">Jackets</a></li>
-                                                        <li><a href="">T-shirts</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="menu_item_children"><a href="#"> Wheels & Tires  <i class="fa fa-angle-right"></i></a>
-                                            <ul class="categories_mega_menu column_3">
-                                                <li class="menu_item_children"><a href="#">Chair</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Dining room</a></li>
-                                                        <li><a href="">bedroom</a></li>
-                                                        <li><a href=""> Home & Office</a></li>
-                                                        <li><a href="">living room</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">Lighting</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Ceiling Lighting</a></li>
-                                                        <li><a href="">Wall Lighting</a></li>
-                                                        <li><a href="">Outdoor Lighting</a></li>
-                                                        <li><a href="">Smart Lighting</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">Sofa</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Fabric Sofas</a></li>
-                                                        <li><a href="">Leather Sofas</a></li>
-                                                        <li><a href="">Corner Sofas</a></li>
-                                                        <li><a href="">Sofa Beds</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="menu_item_children"><a href="#"> Furnitured & Decor <i class="fa fa-angle-right"></i></a>
-                                            <ul class="categories_mega_menu column_2">
-                                                <li class="menu_item_children"><a href="#">Brake Tools</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Driveshafts</a></li>
-                                                        <li><a href="">Spools</a></li>
-                                                        <li><a href="">Diesel </a></li>
-                                                        <li><a href="">Gasoline</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">Emergency Brake</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Dolls for Girls</a></li>
-                                                        <li><a href="">Girls' Learning Toys</a></li>
-                                                        <li><a href="">Arts and Crafts for Girls</a></li>
-                                                        <li><a href="">Video Games for Girls</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li class="menu_item_children"><a href="#"> Turbo System <i class="fa fa-angle-right"></i></a>
-                                            <ul class="categories_mega_menu column_2">
-                                                <li class="menu_item_children"><a href="#">Check Trousers</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Building</a></li>
-                                                        <li><a href="">Electronics</a></li>
-                                                        <li><a href="">action figures </a></li>
-                                                        <li><a href="">specialty & boutique toy</a></li>
-                                                    </ul>
-                                                </li>
-                                                <li class="menu_item_children"><a href="#">Calculators</a>
-                                                    <ul class="categorie_sub_menu">
-                                                        <li><a href="">Dolls for Girls</a></li>
-                                                        <li><a href="">Girls' Learning Toys</a></li>
-                                                        <li><a href="">Arts and Crafts for Girls</a></li>
-                                                        <li><a href="">Video Games for Girls</a></li>
-                                                    </ul>
-                                                </li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="#"> Lighting</a></li>
-                                        <li><a href="#"> Accessories</a></li>
-                                        <li><a href="#">Body Parts</a></li>
-                                        <li><a href="#">Perfomance Filters</a></li>
-                                        <li><a href="#"> Engine Parts</a></li>
-                                        <li class="hidden"><a href="shop-left-sidebar.html">New Sofas</a></li>
-                                        <li class="hidden"><a href="shop-left-sidebar.html">Sleight Sofas</a></li>
-                                        <li><a href="#" id="more-btn"><i class="fa fa-plus" aria-hidden="true"></i> More Categories</a></li>
+                                            @else
+                                                <li><a href="/c/{{$category->slug}}"> {{$category->name}}</a></li>
+                                            @endif
+                                        @endforeach
                                     </ul>
                                 </div>
                             </div>
@@ -418,18 +311,6 @@
                                         <li>
                                             <a class="active" href="/">Главная</a>
                                         </li>
-                                        @foreach($categories as $category)
-                                            <li>
-                                                <a href="/c/{{$category->slug}}">{{$category->name}} <i class="fa fa-angle-down"></i></a>
-                                                @if($category->children->count() > 0)
-                                                    <ul class="sub_menu pages">
-                                                        @foreach($category->children as $child)
-                                                            <li><a href="/c/{{$child->slug}}">{{$child->name}}</a></li>
-                                                        @endforeach
-                                                    </ul>
-                                                @endif
-                                            </li>
-                                        @endforeach
                                         <li>
                                             <a href="/payments-delivery">Доставка и оплата</a>
                                         </li>
