dc92748f5d0aeaa426d493a3ff3b3433549e67ed
talgat <talgat.baltasov@gmail.com>
2020-07-11 17:43:28 +0600

    fixing

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row justify-content-center">
         <div class="col-md-8">
             {!!Form::open(['route' => 'admin.products.store', 'file' => true])!!}
                 <div class="form-group">
