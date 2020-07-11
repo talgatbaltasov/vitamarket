98463f7cdbf35e31404f92a24a57234d826a8746
talgat <talgat.baltasov@gmail.com>
2020-07-11 17:44:26 +0600

    fixing

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Добавить товар</div>
+                <div class="card-header">Добавить</div>
                 <div class="card-body">
                     {!!Form::open(['route' => 'admin.products.store', 'file' => true])!!}
                         <div class="form-group">
