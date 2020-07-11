a692d39284a57da54ca7cebd8ba9cc7f51981a98
talgat <talgat.baltasov@gmail.com>
2020-07-11 18:45:40 +0600

    fixing

--- app/Http/Controllers/Admin/ProductImageController.php
+++ app/Http/Controllers/Admin/ProductImageController.php
@@ -36,7 +36,7 @@ class ProductImageController extends Controller
                 'image'         => $filename
             ]);
             
-            Image::make($image)->resize(870, 400)->save(public_path('/products/'.$filename));
+            Image::make($image)->resize(870, 400)->save(public_path('/images/products/'.$filename));
             $i++;
         }
 
