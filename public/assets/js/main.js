a0bb4e03f51b39f83784e0c35da3b86a3425d964
talgat <talgat.baltasov@gmail.com>
2020-07-12 21:23:39 +0600

    fixing

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -395,7 +395,7 @@
 	 $('[data-countdown]').each(function() {
 		var $this = $(this), finalDate = $(this).data('countdown');
 		$this.countdown(finalDate, function(event) {
-		$this.html(event.strftime('<div class="countdown_area"><div class="single_countdown"><div class="countdown_number">%D</div><div class="countdown_title">days</div></div><div class="single_countdown"><div class="countdown_number">%H</div><div class="countdown_title">hours</div></div><div class="single_countdown"><div class="countdown_number">%M</div><div class="countdown_title">mins</div></div><div class="single_countdown"><div class="countdown_number">%S</div><div class="countdown_title">secs</div></div></div>'));     
+		$this.html(event.strftime('<div class="countdown_area"><div class="single_countdown"><div class="countdown_number">%D</div><div class="countdown_title">дни</div></div><div class="single_countdown"><div class="countdown_number">%H</div><div class="countdown_title">часы</div></div><div class="single_countdown"><div class="countdown_number">%M</div><div class="countdown_title">минуты</div></div><div class="single_countdown"><div class="countdown_number">%S</div><div class="countdown_title">секунд</div></div></div>'));     
                
        });
 	});	
