ceb2647791d425791da2c589c8603668b4f60fd0
talgat <talgat.baltasov@gmail.com>
2020-07-12 21:42:31 +0600

    fixing

---
+++ database/migrations/2020_07_12_153844_create_feedback_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedbackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feedback', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id')->nullable();
+            $table->string('name');
+            $table->string('position')->nullable();
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->string('link')->nullable();
+            $table->string('link_label')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feedback');
+    }
+}
