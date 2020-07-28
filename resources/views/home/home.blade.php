01bc599c9681cae7fd5fd2f0ebb3a1ec49cfddc5
talgat <talgat.baltasov@gmail.com>
2020-07-28 23:48:03 +0600

    fixing

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -22,32 +22,6 @@
                     </div>
                 </div>
             @endforeach
-            <div class="single_slider d-flex align-items-center" data-bgimg="/assets/img/slider/slider2.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="slider_content">
-                                <h1>TOP SALE</h1>
-                                <p>Discount <span>20% Off </span> For Lukani Members </p> 
-                                <a class="button" href="shop.html">Discover Now </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="single_slider d-flex align-items-center" data-bgimg="/assets/img/slider/slider3.jpg">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="slider_content">
-                                <h1>Lovely Plants </h1>
-                                <p>Discount <span>20% Off </span> For Lukani Members </p>
-                                <a class="button" href="shop.html">Discover Now </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </section>
     <!--slider area end-->
