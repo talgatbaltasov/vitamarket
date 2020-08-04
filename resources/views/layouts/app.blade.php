eefa2a2b4b132405ef14b850e401e76fcda8ec5c
talgat <talgat.baltasov@gmail.com>
2020-08-04 23:32:20 +0600

    fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
 </head>
 <body>
 
-    <a href="https://api.whatsapp.com/send?phone=77001030110" style="position: fixed; z-index: 999; bottom: 50px; right: 15px;">
+    <a href="https://api.whatsapp.com/send?phone=77001030110" style="position: fixed; z-index: 999; bottom: 35px; right: 15px;">
         <img src="/assets/img/icon/whatsapp.png" alt="" width="50">
     </a>
     <!--header area start-->
