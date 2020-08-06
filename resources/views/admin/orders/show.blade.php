edd46c52e38ba3b828c83224589204a73982edbb
talgat <talgat.baltasov@gmail.com>
2020-08-06 16:20:00 +0600

    fixing

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -8,36 +8,32 @@
         </div>
         <div class="card-body">
             <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <td>ID</td>
-                        <td>Товар</td>
-                        <td>Количество</td>
-                        <td>Цена за 1шт</td>
-                        <td>Общее</td>
-                    </tr>
-                </thead>
-                <tbody>
+                <tr>
+                    <td>ID</td>
+                    <td>Товар</td>
+                    <td>Количество</td>
+                    <td>Цена за 1шт</td>
+                    <td>Общее</td>
+                </tr>
+                @php
+                    $total = 0;
+                @endphp
+                @foreach($order->items as $item)
                     @php
-                        $total = 0;
+                        $total += $item->quantity * $item->price;
                     @endphp
-                    @foreach($order->items as $item)
-                        @php
-                            $total += $item->quantity * $item->price;
-                        @endphp
-                        <tr>
-                            <td>{{$item->id}}</td>
-                            <td>{{$item->product->name}}</td>
-                            <td>{{$item->quantity}}</td>
-                            <td>{{$item->price}} тг.</td>
-                            <td>{{$item->quantity * $item->price}} тг.</td>
-                        </tr>
-                    @endforeach
                     <tr>
-                        <td class="float-right" colspan="4">Итого:</td>
-                        <td>{{$total}} тг.</td>
+                        <td>{{$item->id}}</td>
+                        <td>{{$item->product->name}}</td>
+                        <td>{{$item->quantity}}</td>
+                        <td>{{$item->price}} тг.</td>
+                        <td>{{$item->quantity * $item->price}} тг.</td>
                     </tr>
-                </tbody>
+                @endforeach
+                <tr>
+                    <td class="float-right" colspan="4">Итого:</td>
+                    <td>{{$total}} тг.</td>
+                </tr>
             </table>
         </div>
     </div>
