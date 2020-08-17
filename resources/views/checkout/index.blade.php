2753bb0f7f324585c77043d2cdc7ade7c3b5e909
talgat <talgat.baltasov@gmail.com>
2020-08-17 20:20:11 +0600

    fixing

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -131,6 +131,7 @@
 
         $('document').ready(function(){
             var shipping_types = {{$raw_shipping_types}}
+            console.log(shipping_types);
             $('#shipping_type_id').on('change', function(){
 
             })
