01f9b139e470a5336455392d73a5057d34be550d
talgat <talgat.baltasov@gmail.com>
2020-10-02 19:19:37 +0600

    fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -326,16 +326,6 @@
                             <a class="dg-widget-link" href="http://2gis.kz/almaty/firm/70000001045157133/center/76.892612,43.239186/zoom/16?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=bigMap">Посмотреть на карте Алматы</a><div class="dg-widget-link"><a href="http://2gis.kz/almaty/center/76.892612,43.239186/zoom/16/routeTab/rsType/bus/to/76.892612,43.239186╎Vitamarket, магазин здоровья?utm_medium=widget-source&utm_campaign=firmsonmap&utm_source=route">Найти проезд до Vitamarket, магазин здоровья</a></div><script charset="utf-8" src="https://widgets.2gis.com/js/DGWidgetLoader.js"></script><script charset="utf-8">new DGWidgetLoader({"width":470,"height":400,"borderColor":"#a3a3a3","pos":{"lat":43.239186,"lon":76.892612,"zoom":16},"opt":{"city":"almaty"},"org":[{"id":"70000001045157133"}]});</script><noscript style="color:#c00;font-size:16px;font-weight:bold;">Виджет карты использует JavaScript. Включите его в настройках вашего браузера.</noscript>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-4 col-sm-6">
-                        <div class="widgets_container widget_menu">
-                            <h3>Служба поддержки</h3>
-                            <div class="footer_menu">
-                                <ul>
-                                    <li><a href="/contacts">Контакты</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
