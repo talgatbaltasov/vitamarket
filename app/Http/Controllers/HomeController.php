7e9f136d0bef5c8d5fdd9e9c38aacebe8c3afa89
talgat <talgat.baltasov@gmail.com>
2022-01-23 19:36:16 +0600

    fixing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,9 @@ class HomeController extends Controller
 
     public function search(Request $request)
     {
-        $products = Product::where('name', 'like', '%'.$request->search.'%')->orWhere('slug', 'like', '%'.$request->search.'%')->paginate(20);
+        $products = Product::where('in_stock', 1)->where(function($q) use($request){
+            $q->where('name', 'like', '%'.$request->search.'%')->orWhere('slug', 'like', '%'.$request->search.'%');
+        })->paginate(20);
         return view('home.search', compact('products'));
     }
 
