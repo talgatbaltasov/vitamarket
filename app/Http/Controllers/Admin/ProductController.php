289a2fcd3240aaddbc5160e0a41f70d812d0fa4c
talgat <talgat.baltasov@gmail.com>
2022-01-23 19:32:54 +0600

    fixing

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -55,7 +55,7 @@ class ProductController extends Controller
     public function update(Product $product, Request $request)
     {
         $data = $request->all();
-        if($request->has('in_stock')) { 
+        if($request->has('in_stock') && $request->in_stock == 1) { 
             $data['in_stock'] = 1;
         } else {
             $data['in_stock'] = 0;
